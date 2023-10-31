268048db59065a3fb766cc1773767625a638f859
Prasann0009 <130238044+Prasann0009@users.noreply.github.com>
2023-10-31 21:31:11 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 //your JS code here. If required.
+ const cont = document.querySelector(".container");
+
+for(let i=1;i<=800;i++)
+{
+  const box = document.createElement("div");
+  box.setAttribute('class','square');
+  cont.appendChild(box);
+}
